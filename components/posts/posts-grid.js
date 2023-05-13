af1f50d863e650fd08fcda22b9f8647a20269bc9
mstfakks <msfa.akkas@gmail.com>
2023-05-13 16:19:22 +0300

    Featured posts and All posts pages created and added some dummy data

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -5,7 +5,7 @@ export default function PostsGrid({ posts }) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem {...post} />
+        <PostItem key={post.slug} {...post} />
       ))}
     </ul>
   );
