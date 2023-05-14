1bc127710e43363ee6bf6c5265b2893dce0d4403
mstfakks <msfa.akkas@gmail.com>
2023-05-14 16:19:34 +0300

    Post detail content arranged and added some styles for code snppets

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,21 +1,57 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started With NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is a first post",
-};
+export default function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,26 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-export default function SinglePostDetail() {
-  return <PostContent />;
+export default function SinglePostDetail({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const { slug } = context?.params;
+  const singlePostData = getPostData(slug);
+  return {
+    props: {
+      post: singlePostData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths(context) {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
