962bd128233be5722862a3d84e1ef53f12d3d16d
mstfakks <msfa.akkas@gmail.com>
2023-05-19 19:52:56 +0300

    db connected and notification added

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -19,7 +21,28 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://mstfakks:M.581441.a@cluster0.dr31itw.mongodb.net/blog-comments?retryWrites=true&w=majority"
+      );
+    } catch (err) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (err) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
